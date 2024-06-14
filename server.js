afb19126fa7db722c31ea4712469cc0d5162f1c2
ALagu Aravind <aalagu748@gmail.com>
2024-06-14 18:53:18 +0530

    update

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ import cookieParser from "cookie-parser";
 import cloudinary from "cloudinary";
 import helmet from "helmet";
 import mongoSanitize from "express-mongo-sanitize";
+const cors = require('cors');
 
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 // routers
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
@@ -43,17 +47,17 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/api/v1/test", (req, res) => {
+app.get("https://careervista-nv0n.onrender.com/api/v1/test", (req, res) => {
   res.json({ msg: "test route" });
 });
 
-app.use("/api/v1/jobs", authenticateUser, jobRouter);
-app.use("/api/v1/users", authenticateUser, userRouter);
-app.use("/api/v1/auth", authRouter);
+app.use("https://careervista-nv0n.onrender.com/api/v1/jobs", authenticateUser, jobRouter);
+app.use("https://careervista-nv0n.onrender.com/api/v1/users", authenticateUser, userRouter);
+app.use("https://careervista-nv0n.onrender.com/api/v1/auth", authRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+// });
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
